f68ca824a938b3c90cfef95d5e7639012a142e29
HarshaHR <harsha.august6@gmail.com>
2023-07-17 19:40:01 +1000

    checking auto deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Developing App FOr Family Choice
         </a>
       </header>
     </div>
