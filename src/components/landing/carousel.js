16748e79be7af215ebbcce1d2ac937dc216859b9
HarshaHR <harsha.august6@gmail.com>
2023-07-18 21:16:30 +1000

    added responsive

--- src/components/landing/carousel.js
+++ src/components/landing/carousel.js
@@ -4,10 +4,15 @@ import alcohol from "../../images/alcohol.jpg";
 import food from "../../images/food.jpg";
 import cigar from "../../images/cigar.jpg";
 import Image from "react-bootstrap/Image";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledImage = styled(Image)`
-  height: 600px;
+height:300px;
+@media only screen and (min-width: 768px) {
+
+    height: 600px;
+}
+  
 `;
 
 const CarouselSection = () => {
--- src/components/landing/catalog.js
+++ src/components/landing/catalog.js
@@ -14,52 +14,61 @@ const StyledContainer = styled(Container)`
     padding-top:1rem;
 `
 
+const StyledCard = styled(Card)`
+align-items:center;
+img{
+    height:150px;
+    width:150px;
+}
+
+`
+
 export const Catalog = () => {
   return (
     <StyledContainer fluid>
     <CardGroup>
-      <Card className="text-center">
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={cokecola}/>
         <Card.Body>
           <Card.Title>Fizzy Drinks</Card.Title>
         </Card.Body>
-      </Card>
-      <Card className="text-center">
+      </StyledCard>
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={catering} />
         <Card.Body>
           <Card.Title >Groceries</Card.Title>
         </Card.Body>
-      </Card>
-      <Card className="text-center">
+      </StyledCard>
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={dental} />
         <Card.Body>
           <Card.Title>Dental Care</Card.Title>
         </Card.Body>
-      </Card>
-      <Card className="text-center">
+      </StyledCard>
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={dips}/>
         <Card.Body>
           <Card.Title>Snacks & Dips</Card.Title>
         </Card.Body>
-      </Card>
-      <Card className="text-center">
+      </StyledCard>
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={softdrinks}/>
         <Card.Body>
           <Card.Title>Soft Drinks</Card.Title>
         </Card.Body>
-      </Card>
-      <Card className="text-center">
+      </StyledCard>
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={alcohol}/>
         <Card.Body>
           <Card.Title>Alcohol</Card.Title>
         </Card.Body>
-      </Card>
-      <Card className="text-center">
+      </StyledCard>
+      <StyledCard className="text-center">
         <Card.Img variant="top" src={softdrinks}/>
         <Card.Body>
           <Card.Title>Soft Drinks</Card.Title>
         </Card.Body>
-      </Card>
+      </StyledCard>
     </CardGroup>
     </StyledContainer>
   )
